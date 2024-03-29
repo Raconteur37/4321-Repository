f1796831b6d52e59db2e709af898f8d19897e156
GuoJiechang <463587571@qq.com>
2024-03-29 13:51:42 -0500

    Add GetRoomTime, configure grabbable plant lifetime flag
    1. Add GetRoomTime script, getting server room time from Realtime object and display on a world canvas.
    2. Disable option: 'Destroy When Last Client Leaves', so that the changes over the plant can remain even though all client leaves.

---
+++ Assets/GetRoomTime.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+using Normal.Realtime;
+using System;
+
+public class GetRoomTime : MonoBehaviour
+{
+    public TMP_Text text = null;
+    public GameObject realtimeGO = null;
+
+    private Realtime realtime = null;
+
+    private void Start()
+    {
+        realtime = realtimeGO.GetComponent<Realtime>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(text != null && realtime != null)
+        {
+            //DateTime realTime = DateTimeOffset.FromUnixTimeSeconds(realtime.roomTime).UtcDateTime;
+            text.text = "Room time: " + realtime.roomTime.ToString();
+        }
+    }
+}
