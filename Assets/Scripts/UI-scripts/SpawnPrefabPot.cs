49ca077a5888f3f15f5267782f6b6adb2cfbd815
kurmanbek <kurmanbekb001@gmail.com>
2024-04-21 21:48:44 -0500

    fixed script

--- Assets/Scripts/UI-scripts/SpawnPrefabPot.cs
+++ Assets/Scripts/UI-scripts/SpawnPrefabPot.cs
@@ -3,21 +3,16 @@ using UnityEngine;
 public class SpawnPrefabPot : MonoBehaviour
 {
     public GameObject prefabToSpawn; // Assign this in the inspector
-    private float spawnHeight = 0.5f; // Height to spawn above the ground
-    private float currentHeight = 0f; // Current height for spawning
+    [SerializeField] private Transform WristMenuTransform;
+    [SerializeField] private Vector3 offsetFromWristMenu = new Vector3(0f, 0.1f, 0f);
 
     // Method to instantiate the prefab
     public void SpawnObject()
     {
         if (prefabToSpawn != null && WristMenuTransform != null)
         {
-            // Increment the current height
-            currentHeight += spawnHeight;
-            
-            // Calculate the spawn position with the incremented height
-            Vector3 spawnPosition = new Vector3(0, currentHeight, -4);
-            
-            // Instantiate the prefab at the calculated position
+            // Calculate the spawn position relative to the wrist menu with the offset
+            Vector3 spawnPosition = WristMenuTransform.TransformPoint(offsetFromWristMenu);
             Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
         }
         else
